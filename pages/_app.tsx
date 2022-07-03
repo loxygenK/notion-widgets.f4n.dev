47e707ea4a95ff59afd0c5382c3ba3fef181a6e8
Flisan <me@loxygen.dev>
2022-07-03 16:20:42 +0900

    ✨ Create wrapper and other components (#1)
    * 🔧 Configure alias for root
    
    * 🎨 Add configurations for primary colors
    
    * 🍱 Add logo asset
    
    * 🧑💻 Create function for fight against empty className
    
    * ✨ Create some primitive components
    
    * ♻ Simplify page structure and move global things to `_app.tsx`
    
    * 🐛 Fix invalid import path
    
    * ♻ Make wrapping selective

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import { Header } from "~/components/molecules/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>notion-widgets.f4n.dev</title>
+        <meta name="description" content="Notion widgets hoster" />
+        {/* TODO: Replace this path specification to .ico files */}
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
