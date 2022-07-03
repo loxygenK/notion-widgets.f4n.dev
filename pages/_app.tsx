6e5f01c3491037269b2a6067c0cf0c1615b3fb89
loxygenk <me@loxygen.dev>
2022-07-03 16:04:32 +0900

    ♻ Simplify page structure and move global things to `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import { Header } from "~/components/molecules/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>notion-widgets.f4n.dev</title>
+        <meta name="description" content="Notion widgets hoster" />
+        {/* TODO: Replace this path specification to .ico files */}
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+
+      <Header />
+      <main className="p-12">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
 export default MyApp;
